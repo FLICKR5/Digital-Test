7f24ed54f49ffa9ecf91f4a297afc8629d53a9f2
Tushar Srivastava <srivastavatushar29@gmail.com>
2020-08-14 00:36:39 +0530

    Just create a Database and a Table

--- back-end/main.py
+++ back-end/main.py
@@ -1,1 +1,9 @@
-print("Digital-Test Back-End Program")
+import sqlite3
+
+conn = sqlite3.connect('Quiz.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Teachers')
+cur.execute('CREATE TABLE Teachers (Questions TEXT, Options TEXT, Answer TEXT)')
+
+conn.close()
