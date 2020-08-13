8500841c691e8f80c1fc8ba44fa97e6ce7389917
Tushar Srivastava <srivastavatushar29@gmail.com>
2020-08-14 01:02:14 +0530

    Create Database and a Table

--- back-end/main.py
+++ back-end/main.py
@@ -1,4 +1,10 @@
 import sqlite3
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 conn = sqlite3.connect('Quiz.sqlite')
 cur = conn.cursor()
@@ -6,5 +12,4 @@ cur = conn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS Teacher
             (Questions TEXT, Options TEXT, Answers TEXT)''')
 
-cur.close()
 conn.close()
