c5f5d2df194e62d2a00a5bf04616e96552400165
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2020-08-13 15:21:55 +0530

    Creaded a new window
    A Blank Qt5 window at (200, 100) and with width 900 and height 600

--- front-end/main.py
+++ front-end/main.py
@@ -1,1 +1,14 @@
-print("Digital Test Front-End")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 100, 900, 600)
+    win.setWindowTitle("Digital Test")
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
