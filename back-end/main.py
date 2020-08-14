af952e128d0b872e95e45bbff30f690034dcbcf2
Tushar Srivastava <srivastavatushar29@gmail.com>
2020-08-15 01:21:59 +0530

    Create Database and Two Tables

--- back-end/main.py
+++ back-end/main.py
@@ -1,19 +1,18 @@
 import sqlite3
-import ssl
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 # Create Database
-conn = sqlite3.connect('Quiz.sqlite')
+conn = sqlite3.connect('Digital-Test.sqlite')
 cur = conn.cursor()
 
 # Create Table
 cur.execute('''CREATE TABLE IF NOT EXISTS Teacher
-            (Id INTEGER PRIMARY KEY, Questions TEXT, Options TEXT, Answers TEXT)''')
-cur.execute('''CREATE TABLE IF NOT EXISTS Student
-            (Id INTEGER PRIMARY KEY, Questions TEXT, Options TEXT)''')
+            (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+             Questions TEXT,
+             Options TEXT,
+             Answers TEXT)''')
 
-conn.close()
+cur.execute('''CREATE TABLE IF NOT EXISTS Student
+            (Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 
+             Questions TEXT,
+             Options TEXT)''')
+conn.commit()
