8043202a0490f39c7c5e260367434be78c6c2e19
Tushar Srivastava <48978533+Tushar8645@users.noreply.github.com>
2020-08-14 09:25:38 +0530

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -6,9 +6,11 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Create Database
 conn = sqlite3.connect('Quiz.sqlite')
 cur = conn.cursor()
 
+# Create Table
 cur.execute('''CREATE TABLE IF NOT EXISTS Teacher
             (Questions TEXT, Options TEXT, Answers TEXT)''')
 
