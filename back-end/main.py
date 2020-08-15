d3b8706ef46dd4c4c15843b4c97180519e5f1923
Tushar Srivastava <srivastavatushar29@gmail.com>
2020-08-15 20:38:10 +0530

    Insert Record

--- back-end/main.py
+++ back-end/main.py
@@ -17,27 +17,24 @@ def Insert_Sqlite_Table():
         while True:
             n += 1
 
-            choice = input("What do you want to do ('INSERT, UPDATE, DROP, QUIT'): ")
-
-            if choice == 'INSERT' or choice  == 'Insert' or choice == 'insert':
-                Ques = input('Enter the {} question: '.format(n))
-                Options_1 = input('Enter the {} Option: '.format(n))
-                Options_2 = input('Enter the {} Option: '.format(n + 1))
-                Options_3 = input('Enter the {} Option: '.format(n + 2))
-                Options_4 = input('Enter the {} Option: '.format(n + 3))  
-                Ans = input('Enter the correct answer: '.format(n))
+            Ques = input('Enter the {} question: '.format(n))
+            Options_1 = input('Enter the {} Option: '.format(n))
+            Options_2 = input('Enter the {} Option: '.format(n + 1))
+            Options_3 = input('Enter the {} Option: '.format(n + 2))
+            Options_4 = input('Enter the {} Option: '.format(n + 3))  
+            Ans = input('Enter the correct answer: '.format(n))
 
 
-                cur.execute('''INSERT INTO Teacher (Questions, Options_1, Options_2, Options_3, Options_4, Answers)
-                            VALUES (?, ?, ?, ?, ?, ?)''', (Ques, Options_1, Options_2, Options_3, Options_4, Ans))
+            cur.execute('''INSERT INTO Teacher (Questions, Options_1, Options_2, Options_3, Options_4, Answers)
+                        VALUES (?, ?, ?, ?, ?, ?)''', (Ques, Options_1, Options_2, Options_3, Options_4, Ans))
 
-                conn.commit()
+            conn.commit()
 
-                check = input('Do you want to continue or not (Y/N): ')
-                if check == 'Y' or check == 'y':
-                    continue
-                else:
-                    break
+            check = input('Do you want to continue or not (Y/N): ')
+            if check == 'Y' or check == 'y':
+                continue
+            else:
+                break
 
     except sqlite3.Error as error:
         print('Failed to insert data into sqlite table', error)
