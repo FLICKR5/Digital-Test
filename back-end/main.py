7dbc62a0be2267cdd50d609efbb491c2a00d9b70
Tushar Srivastava <srivastavatushar29@gmail.com>
2020-08-16 11:20:31 +0530

    Add update, delete and drop function and fix issues

--- back-end/main.py
+++ back-end/main.py
@@ -10,15 +10,18 @@ def Insert_Sqlite_Table(ques, opt1, opt2, opt3, opt4, ans):
         # Create Table
         cur.execute('''CREATE TABLE IF NOT EXISTS Teacher
                     (Ques_No INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    ques TEXT,
-                    opt1 TEXT, opt2 TEXT, opt3 TEXT, opt4 TEXT,
-                    ans TEXT)''')
+                    Questions TEXT,
+                    Option_1 TEXT, Option_2 TEXT, Option_3 TEXT, Option_4 TEXT,
+                    Answers TEXT)''')
 
-        cur.execute('''INSERT INTO Teacher (Questions, Options_1, Options_2, Options_3, Options_4, Answers)
+        cur.execute('''INSERT INTO Teacher (Questions, Option_1, Option_2, Option_3, Option_4, Answers)
                     VALUES (?, ?, ?, ?, ?, ?)''', (ques, opt1, opt2, opt3, opt4, ans))
 
         conn.commit()
 
+        print("Record inserted successfully into Teacher table", cur.rowcount)
+
+        cur.close()
 
     except sqlite3.Error as error:
         print('Failed to insert data into sqlite table', error)
@@ -28,15 +31,124 @@ def Insert_Sqlite_Table(ques, opt1, opt2, opt3, opt4, ans):
             print('The SQLite connection is closed')
 
 
+def update_sqlite_table(ques, opt1, opt2, opt3, opt4, ans, ques_no):
+    try:
+        conn = sqlite3.connect('Digital-Test.sqlite')
+        cur = conn.cursor()
+        print("Connected to SQLite")
+
+        cur.execute('''SELECT * FROM Teacher WHERE Ques_No = ?''', (ques_no, ))
+        print("Reading single row")
+
+        try:
+            cur.fetchone()
+            cur.execute('''UPDATE Teacher SET Questions = ?, Option_1 = ?, Option_2 = ?, Option_3 = ?, Option_4 = ?, Answers = ?
+                            WHERE Ques_NO = ?''', (ques, opt1, opt2, opt3, opt4, ans, ques_no))
+
+            print("Total", cur.rowcount, "Records updated successfully")
+
+        except:
+            cur.execute('''INSERT INTO Teacher (Ques_No, Questions, Option_1, Option_2, Option_3, Option_4, Answer)
+                            VALUES (?, ?, ?, ?, ?, ?, ?)''', (ques_no, ques, opt1, opt2, opt3, opt4, ans))
+
+            print("Record inserted successfully into Teacher table", cur.rowcount)
+
+        conn.commit()
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("Failed to update multiple records of sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+            print("The SQLite connection is closed")
+
+
+def delete_row(ques_no):
+    try:
+        conn = sqlite3.connect('Digital-Test.sqlite')
+        cur = conn.cursor()
+        print("Connected to SQLite")
+
+        cur.execute('''DELETE FROM Teacher WHERE Ques_No = ?''', (ques_no, ))
+
+        conn.commit()
+
+        print("Record deleted successfully")
+
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("Failed to delete reocord from a sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+            print("sqlite connection is closed")
+
+
+def drop_sqlite_table():
+    try:
+        conn = sqlite3.connect('Digital-Test.sqlite')
+        cur = conn.cursor()
+        print("Connected to SQLite")
+
+        cur.execute('''DROP TABLE Teacher''')
+
+        conn.commit()
+
+        print("Table Dropped")
+
+        cur.close()
+
+    except sqlite3.Error as error:
+        print("Failed to drop table from a sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+            print("sqlite connection is closed")
+
+
 if __name__ == '__main__':
-    for i in range(0, 6):
+    print("\n ***** Welcome Teachers ***** \n")
+    print("1. Insert question")
+    print("2. Update question")
+    print("3. Delete question")
+    print("4. Delete data")
+    print("5. Exit \n")
+    
+    val = input("Select your choice: ")
 
-        n = 1
-        ques = input('Enter the {} question: '.format(i + 1))
-        opt1 = input('Enter the {} Option: '.format(n))
-        opt2 = input('Enter the {} Option: '.format(n + 1))
-        opt3 = input('Enter the {} Option: '.format(n + 2))
-        opt4 = input('Enter the {} Option: '.format(n + 3))  
-        ans = input('Enter the correct answer: '.format(n))
+    if val == '1':
+        for i in range(0, 3):
+            n = 1
+            ques = input('Enter the {} question: '.format(i + 1))
+            opt1 = input('Enter the {} Option: '.format(1))
+            opt2 = input('Enter the {} Option: '.format(2))
+            opt3 = input('Enter the {} Option: '.format(3))
+            opt4 = input('Enter the {} Option: '.format(4))  
+            ans = input('Enter the correct answer: ')
 
-        Insert_Sqlite_Table(ques, opt1, opt2, opt3, opt4, ans)
+            Insert_Sqlite_Table(ques, opt1, opt2, opt3, opt4, ans)
+
+    if val == '2':
+        ques_no = input('Enter the question number: ')
+        
+        ques = input('Enter the {} question: '.format(ques_no))
+        opt1 = input('Enter the {} Option: '.format(1))
+        opt2 = input('Enter the {} Option: '.format(2))
+        opt3 = input('Enter the {} Option: '.format(3))
+        opt4 = input('Enter the {} Option: '.format(4))  
+        ans = input('Enter the correct answer: ')
+
+        update_sqlite_table(ques, opt1, opt2, opt3, opt4, ans, ques_no)
+
+    if val == '3':
+        ques_no = input('Enter the row number which you want to delete: ')
+        delete_row(ques_no)
+
+    if val == '4':
+        drop_sqlite_table()
+
+    if val == '5':
+        print('Thanks for vising here')
+        quit()
