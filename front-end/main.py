e054c1b28915e3378adef88bb53622a7e4227dac
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2020-08-26 11:00:21 +0530

    Getting started with kivy

--- front-end/main.py
+++ front-end/main.py
@@ -1,8 +1,41 @@
+import kivy
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2 
+        self.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline = False)
+        self.add_widget(self.name)
 
-TestApp().run()
+        self.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline = False)
+        self.add_widget(self.email)
+
+        self.add_widget(Label(text="Passwod: "))
+        self.password = TextInput(multiline = False)
+        self.add_widget(self.password)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        print(self.name.text)
+        print(self.email.text)
+        print(self.password.text)
+        self.name.text=""
+        self.email.text=""
+        self.password.text=""
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
